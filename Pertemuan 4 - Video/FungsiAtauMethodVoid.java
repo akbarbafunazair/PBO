e293f195d34e5e392a09ed516cc788a446510bc6
Akbar Bafunazair <72426613+akbarbafunazair@users.noreply.github.com>
2020-10-12 23:15:32 +0700

    Pertemuan 4 - Video

---
+++ Pertemuan 4 - Video/FungsiAtauMethodVoid.java
@@ -0,0 +1,27 @@
+public class FungsiAtauMethodVoid {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(simpel());
+
+        fungsiVoid("apa pun");
+        selamatPagi("emak");
+        selamatPagi("abah");
+
+    }
+
+    private static void selamatPagi(String nama){
+        System.out.println("selamat pagi " + nama);
+    }
+
+
+    private static void fungsiVoid(String input){
+        System.out.println(input);
+    }
+
+    private static float simpel(){
+        return 10.0f;
+    }
+}
+
