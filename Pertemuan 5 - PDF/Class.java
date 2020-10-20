2965770ea593a9d2dfcec63b5195dc04db7d6101
Akbar Bafunazair <72426613+akbarbafunazair@users.noreply.github.com>
2020-10-20 21:27:56 +0700

    Pertemuan 5 - PDF

---
+++ Pertemuan 5 - PDF/Class.java
@@ -0,0 +1,10 @@
+public class Class {
+    public int x=5;//attribut ini tidak boleh diakses secara langsung dari lluar classku
+    int y = 15;
+    private int z = 20;
+
+    //method
+    public int tampilkanZ(){
+        return this.z;
+    }
+}
